cb09dd0a8d2757db64db973f5a5250bbf1549f18
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-06-23 22:07:13 +0530

    added crud operations

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,20 @@
 class Friend < ApplicationRecord
+  validates :name, length: { minimum: 2 }
+  validates :phone, length: { is: 10 }, numericality: true
+  validates :github, length: { minimum: 12,  }
+  validate :github_must_have_github
+  validates :email, length: { minimum: 2 }
+  validate :email_must_have_at_symbol
+  validates :hobby, length: { minimum: 5 }
+  validates :location, length: { minimum: 2 }
+
+  private
+
+  def email_must_have_at_symbol
+    errors.add(:email, "must include @ symbol") unless email.include?("@")
+  end
+
+  def github_must_have_github
+    errors.add(:github, "must include github.com/") unless github.include?("github.com/")
+  end
 end
